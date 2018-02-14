963c951b82d14ad8d29840c70fd966bc9ae0da7f
CINDY <jkpark@innerbus.com>
2018-02-14 16:22:24 +0900

    AI-3.0.1 <Cindy@CINDY-PC Create IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, other.xml, GoogleStyle copy.xml, gradle.experimental.xml, Default.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, keymap.xml, androidEditors.xml, gradle.run.settings.xml, code.style.schemes.xml, notifications.xml, github_settings.xml, InnerBeansFileHeader.java, bitbucket-server-registry.xml, Default.xml, laf.xml, jdk.table.xml, debugger.renderers.xml, path.macros.xml, Google Style.xml, web-browsers.xml, vcs.xml, bitbucket-cloud-registry.xml, editor.codeinsight.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/InnerBeansFileHeader.java
@@ -0,0 +1,10 @@
+#set ($EMAIL = "jkpark@innerbus.com")
+/**
+ * Main System : InnerBeans Android
+ 
+ * Created by ${USER}(${EMAIL}) on ${DATE} ${TIME}
+ 
+ * Description
+ *
+ *Copyright (c)${YEAR} by INNERBUS Co.Ltd. Allright reserved.
+ */
